72f5b735bc7059dd6f7572567d1f1244f25dccef
Mikhail <mihantoff@yandex.ru>
2020-04-20 13:42:56 -0800

    Please enter the commit message for your changes. Lines starting First commit
    :wq

---
+++ src/main/java/com/example/GreetingController.java
@@ -0,0 +1,64 @@
+package com.example;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.domain.User;
+import com.example.repo.UserRepo;
+
+@Controller
+public class GreetingController {
+    @Autowired
+    private UserRepo userRepo;
+    
+    
+
+    public GreetingController(UserRepo userRepo) {
+		
+		this.userRepo = userRepo;
+	}
+
+	
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        Iterable<User> users = userRepo.findAll();
+
+        model.put("users", users);
+
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String name, @RequestParam String email, Map<String, Object> model) {
+        
+        User user = new User  (name, email);
+
+        userRepo.save(user);
+
+        Iterable<User> users = userRepo.findAll();
+
+        model.put("users", users);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<User> users;
+
+        if (filter != null && !filter.isEmpty()) {
+            users = userRepo.findByName(filter);
+        } else {
+        	users = userRepo.findAll();
+        }
+
+        model.put("users", users);
+
+        return "main";
+    }
+}
---
+++ src/main/java/com/example/repo/UserRepo.java
@@ -0,0 +1,15 @@
+package com.example.repo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import com.example.domain.User;
+
+
+public interface UserRepo extends CrudRepository <User, Integer> {
+	
+	List<User> findByName (String name);
+
+}
